dedea6794e64933403f9891dcde11b71447bf233
shivamshukla12 <88490443+shivamshukla12@users.noreply.github.com>
2023-07-23 14:05:59 +0530

    HANA Cloud GET CALL

--- srv/SupplierData.js
+++ srv/SupplierData.js
@@ -6,35 +6,5 @@ module.exports = cds.service.impl(async function () {
 const getSupplier = async (req) => {
   const srv = await cds.connect.to("db");
   const { Supplier } = srv.entities;
-
-  let venQuery1 = SELECT.columns("ID").from(`${Supplier.name}`);
-  let result1 = await srv.run(venQuery1);
-  return result1;
-
-  // return await db.run(req.query);
-  // res = {};
-  // records = [];
-  // res.ID = "1";
-  // res.FirstName = "James";
-  // res.LastName = "Bond";
-  // res.Address = "CA Aaron 76 Street No 1";
-  // res.ZipCode = "+960056";
-  // res.Country = "USA";
-  // res.Phone = "+1 9000 102 223";
-  // records.push(res);
-
-  // res.ID = "2";
-  // res.FirstName = "Keanu";
-  // res.LastName = "Reeves";
-  // res.Address = "CA Aaron 76 Street No 2";
-  // res.ZipCode = "+960056";
-  // res.Country = "USA";
-  // res.Phone = "+1 9001 102 223";
-  // records.push(res);
-
-  return supplier;
-
-  /** These are the hard code Values and Added in Entity */
-
-  /** Lets Load Data from CSV*/
+  return await srv.run(SELECT.from(`${Supplier.name}`));
 };
